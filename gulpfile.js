903a872063322add09955e9f45ca716450c43789
willybeans <willwedmedyk@gmail.com>
2017-09-14 22:59:41 -0400

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 
@@ -22,5 +23,6 @@ gulp.task('coffee',function(){
 gulp.task('js', function(){
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
